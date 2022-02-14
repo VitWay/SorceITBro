ec25c34950157dec1974efb983cc6c128bc533a3
Vitalii Kr <vitaliikr@gmail.com>
2022-02-14 13:54:43 +0200

    q

--- Projects/HW 23/main.js
+++ Projects/HW 23/main.js
@@ -4,37 +4,34 @@ const formFileds = form.elements;
 const submitBtn = form.querySelector('[type="submit"]');
 submitBtn.addEventListener('click', clearStorage);
 
-
-
-
-function clearStorage(){
+function clearStorage() {
     localStorage.clear();
 }
 
-function changeHandler(){
-    if(this.type !== 'checkbox'){
+function changeHandler() {
+    if (this.type !== 'checkbox') {
         console.log(this.name, this.value);
         localStorage.setItem(this.name, this.value);
-    } else{
+    } else {
         console.log(this.name, this.checked);
         localStorage.setItem(this.name, this.checked);
     }
 }
 
-function chechStorage(){
-    for(let i = 0; i < formFileds.length; i++){
-        if(formFileds[i].type !== 'submit'){
-            if(formFileds[i].type === 'checked'){
+function chechStorage() {
+    for (let i = 0; i < formFileds.length; i++) {
+        if (formFileds[i].type !== 'submit') {
+            if (formFileds[i].type === 'checked') {
                 formFileds[i].checked = localStorage.getItem(formFileds[i].name);
-            }else{
+            } else {
                 formFileds[i].value = localStorage.getItem(formFileds[i].name)
             }
         }
     }
     attachEvents();
 }
-function attachEvents(){
-    for(let i = 0; i < formFileds.length; i++){
+function attachEvents() {
+    for (let i = 0; i < formFileds.length; i++) {
         formFileds[i].addEventListener('change', changeHandler);
     }
 }
