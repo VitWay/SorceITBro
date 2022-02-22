2604049482b58613bc7b2dffd76ccb615f596d07
Vitalii Kr <vitaliikr@gmail.com>
2022-02-22 16:45:45 +0200

    many changes

--- siteForGraduate/scripts/animNav.js
+++ siteForGraduate/scripts/animNav.js
@@ -1,4 +1,13 @@
-const toggle = document.getElementById('toggle')
-const nav = document.getElementById('nav')
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav-menu");
 
-toggle.addEventListener('click', () => nav.classList.toggle('active'))
+hamburger.addEventListener('click', () => {
+    hamburger.classList.toggle("active");
+    navMenu.classList.toggle("active");
+})
+
+document.querySelectorAll('.nav-link').forEach(n => n.
+    addEventListener('click', () => {
+        hamburger.classList.remove('active');
+        navMenu.classList.remove('active');
+    }))
